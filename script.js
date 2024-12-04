a6976887618ba517cd5f50e125576b3d8e1d1b07
Boris Zapolsky <boris.zapolsky@gmail.com>
2024-12-04 18:29:44 +0200

    Improved the book card design

--- script.js
+++ script.js
@@ -80,18 +80,27 @@ function createCard(obj) {
   } else {
     readIcon = "images/todo.png";
   }
+
+  const bookCovers = ["#f27121", "#e94057", "#8a2387"];
+  const pickCover =
+    myLibrary.length % 3 === 0
+      ? bookCovers[2]
+      : myLibrary.length % 2 === 0
+      ? bookCovers[1]
+      : bookCovers[0];
+
   const card = document.createElement("div");
   card.className = "book-card";
   card.dataset.id = obj.id;
   card.innerHTML = `
-      <div class="book-img">
-          <svg width="64px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>book-open-blank-variant</title><path d="M6.5 20C8.2 20 10.65 20.65 12 21.5C13.35 20.65 15.8 20 17.5 20C19.15 20 20.85 20.3 22.25 21.05C22.35 21.1 22.4 21.1 22.5 21.1C22.75 21.1 23 20.85 23 20.6V6C22.4 5.55 21.75 5.25 21 5C19.89 4.65 18.67 4.5 17.5 4.5C15.55 4.5 13.45 4.9 12 6C10.55 4.9 8.45 4.5 6.5 4.5C5.33 4.5 4.11 4.65 3 5C2.25 5.25 1.6 5.55 1 6V20.6C1 20.85 1.25 21.1 1.5 21.1C1.6 21.1 1.65 21.1 1.75 21.05C3.15 20.3 4.85 20 6.5 20M12 19.5V8C13.35 7.15 15.8 6.5 17.5 6.5C18.7 6.5 19.9 6.65 21 7V18.5C19.9 18.15 18.7 18 17.5 18C15.8 18 13.35 18.65 12 19.5Z" /></svg>
-      </div>
       <div class="book-title">
-          <h4>${obj.title}</h4>
-          <p>
-              ${obj.author}
-          </p>
+          <h2>${obj.title}</h2>
+      </div>
+      <div class="book-img">
+          <svg width="64px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>book-open-blank-variant</title><path fill=${pickCover} d="M6.5 20C8.2 20 10.65 20.65 12 21.5C13.35 20.65 15.8 20 17.5 20C19.15 20 20.85 20.3 22.25 21.05C22.35 21.1 22.4 21.1 22.5 21.1C22.75 21.1 23 20.85 23 20.6V6C22.4 5.55 21.75 5.25 21 5C19.89 4.65 18.67 4.5 17.5 4.5C15.55 4.5 13.45 4.9 12 6C10.55 4.9 8.45 4.5 6.5 4.5C5.33 4.5 4.11 4.65 3 5C2.25 5.25 1.6 5.55 1 6V20.6C1 20.85 1.25 21.1 1.5 21.1C1.6 21.1 1.65 21.1 1.75 21.05C3.15 20.3 4.85 20 6.5 20M12 19.5V8C13.35 7.15 15.8 6.5 17.5 6.5C18.7 6.5 19.9 6.65 21 7V18.5C19.9 18.15 18.7 18 17.5 18C15.8 18 13.35 18.65 12 19.5Z" /></svg>
+      </div>
+      <div class="book-author">
+          <p>by ${obj.author}</p>
       </div>
       <div class="book-pages">
           <small>${obj.pages} pages</small>
